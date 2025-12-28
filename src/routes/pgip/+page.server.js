5772e554fc4e0f7f12bf5f4fc9f8b56a85476b20
3mrld <not3mrld@gmail.com>
2025-12-28 21:29:36 +0700

    add tutorial for iphone

---
+++ src/routes/pgip/+page.server.js
@@ -0,0 +1,5 @@
+import { redirect } from "@sveltejs/kit";
+
+export function load() {
+    redirect(308, 'hhttps://www.facebook.com/pgvnc/posts/pfbid02SgzEXjHShgtAQp5MxRpqYpXd79KopxHg286p1jbQtfoPN1b8UNeW27KRrVeBFFebl')
+}
