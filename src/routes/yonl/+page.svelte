ce740f2f26a3c0a87d4aea03d8d571a1b46d8dd1
3mrld <not3mrld@gmail.com>
2026-02-01 10:11:34 +0700

    add YONL page title

---
+++ src/routes/yonl/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  // You can set the title dynamically or statically
+  const pageTitle = "YONL";
+</script>
+
+<svelte:head>
+  <title>{pageTitle}</title>
+</svelte:head>
+
+<h1>Welcome to the About page</h1>
